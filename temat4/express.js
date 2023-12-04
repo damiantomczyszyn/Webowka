ad56ca49a4b26a059d11c873bce79ac312807a36
Damian Tomczyszyn <damitom@o2.pl>
2023-12-04 11:12:35 +0100

    poprawienie nawiasow

--- temat4/express.js
+++ temat4/express.js
@@ -3,4 +3,5 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello Node and Express!'))
-app.listen(port, () => console.log('Aplikacja uruchomiona na porcie ${port}!'))
+//app.listen(port, () => console.log('Aplikacja uruchomiona na porcie $port}!'))
+app.listen(port, () => console.log(`Aplikacja uruchomiona na porcie ${port}!`));
