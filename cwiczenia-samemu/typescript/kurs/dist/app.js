75691815d9ad10f495fc18f1e7c7d1af304cac56
Damian Tomczyszyn <damitom@o2.pl>
2023-12-09 12:58:37 +0100

    testowe quesry selector

--- cwiczenia-samemu/typescript/kurs/dist/app.js
+++ cwiczenia-samemu/typescript/kurs/dist/app.js
@@ -12,3 +12,10 @@ const add3 = (v1, v2) => {
 console.log(add(10, 40));
 console.log(add2(20));
 console.log(add3(20));
+const input1Element = document.querySelector("#input1");
+const input2Element = document.querySelector("#input2");
+const addButtonElement = document.querySelector("#button");
+addButtonElement.addEventListener("click", () => {
+    const sum = add(input1Element.value, input2Element.value);
+    console.log(sum);
+});
