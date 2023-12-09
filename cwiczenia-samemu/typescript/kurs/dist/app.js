94f7f3e5499af013e7b5236b38550954a231c040
Damian Tomczyszyn <damitom@o2.pl>
2023-12-09 12:55:01 +0100

    parametry opcjonalne

--- cwiczenia-samemu/typescript/kurs/dist/app.js
+++ cwiczenia-samemu/typescript/kurs/dist/app.js
@@ -1,2 +1,14 @@
 console.log("test!");
 let age = 29;
+let ageAsString = "dwadziescia dziewiec";
+ageAsString = `${age}`;
+console.log(ageAsString);
+const add = (v1, v2) => v1 + v2;
+const add2 = (v1, v2 = 5) => v1 + v2;
+const add3 = (v1, v2) => {
+    v2 = v2 !== null && v2 !== void 0 ? v2 : 0;
+    return v1 + v2;
+};
+console.log(add(10, 40));
+console.log(add2(20));
+console.log(add3(20));
--- cwiczenia-samemu/typescript/kurs/src/app.ts
+++ cwiczenia-samemu/typescript/kurs/src/app.ts
@@ -1,3 +1,22 @@
 console.log("test!");
 
 let age: number = 29;
+
+let ageAsString: string = "dwadziescia dziewiec";
+ageAsString = `${age}`;
+
+console.log(ageAsString);
+
+const add = (v1: number, v2: number) => v1 + v2;
+const add2 = (v1: number, v2: number = 5) => v1 + v2;
+const add3 = (v1: number, v2?: number) => {
+    v2 = v2 ?? 0;
+    return v1 + v2
+}
+
+
+console.log(add(10, 40));
+console.log(add2(20));
+console.log(add3(20));
+
+
